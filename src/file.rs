9109ff0866fd2ee4162bcb4f5788c7d0e8b90e10
pan93412 <pan93412@gmail.com>
2022-07-07 01:00:44 +0800

    test(file): integration test for writer & reader

--- src/file.rs
+++ src/file.rs
@@ -6,3 +6,53 @@ pub mod timestamp;
 pub mod writer;
 
 mod datadir;
+
+#[cfg(test)]
+mod tests {
+    use super::{writer::{DataWriter, DataEntry}, reader::FileReader};
+
+    #[tokio::test]
+    async fn test_read_write() {{
+        // Add log to catch errors.
+        tracing_subscriber::fmt::init();
+
+        const CONTENT_A: &[u8] = b"Hello, world!";
+        const CONTENT_B: &[u8] = b"<OwO>";
+        
+        let filename = uuid::Uuid::new_v4().to_string();
+
+        //////////////////
+        // Writer Part ///
+        //////////////////
+
+        // The writer to write today's content;
+        let mut writer = DataWriter::new();
+        let writer_thread = writer.start().await.expect("failed to spawn writer");
+
+        writer.add(DataEntry {
+            filename: filename.to_string(),
+            data: CONTENT_A.to_vec(),
+        }).expect("failed to add CONTENT_A");
+
+        writer.add(DataEntry {
+            filename: filename.to_string(),
+            data: CONTENT_B.to_vec(),
+        }).expect("failed to add CONTENT_B");
+
+        writer.stop().expect("failed to stop writer");
+        writer_thread.await.expect("failed to wait writer thread to stop");
+
+
+        //////////////////
+        // Reader Part ///
+        //////////////////
+
+        // The reader to read today's content.
+        let mut reader = FileReader::new(filename.to_string(), 0)
+            .expect("failed to start the reader to read the written data.");
+        
+        assert_eq!(reader.next().expect("unexpected end (1)"), CONTENT_A);
+        assert_eq!(reader.next().expect("unexpected end (2)"), CONTENT_B);
+        assert!(reader.next().is_none());
+    }}
+}
