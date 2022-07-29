5654331904a169ded482271b61f507adf2e05f4c
pan93412 <pan93412@gmail.com>
2022-07-29 14:48:00 +0800

    refactor!(data/fields): rewrite fields serializer

--- src/data/fields.rs
+++ src/data/fields.rs
@@ -1,7 +1,6 @@
 //! The prefered encoded data structures of `libstock`.
 
 use std::{
-    io::{BufReader, Read},
     str::FromStr,
     time::{SystemTime, SystemTimeError},
 };
@@ -17,45 +16,36 @@ use super::{
         bit_deserialize_message_type, bit_deserialize_trade_side, bit_serialize_message_type,
         bit_serialize_trade_side, DataTypesError, Exchange, InfoType, MARKET_TYPE_BIT, PERIOD,
         SYMBOL_PAIR,
-    },
+    }, serializer::{FieldSerializer, FieldDeserializer},
 };
 
-pub trait ReadExt: Read {
-    /// Read data to a fixed array.
-    fn read_exact_array<const LEN: usize>(&mut self) -> StructureResult<[u8; LEN]> {
-        let mut payload = [0u8; LEN];
-        self.read_exact(&mut payload)
-            .map_err(|e| StructureError::ReadFromReaderFailed(e, LEN))?;
+/// The timestamp of exchange (6 byte).
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ExchangeTimestampField(pub u64);
 
-        Ok(payload)
+impl FieldSerializer<6> for ExchangeTimestampField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 6], Self::Err> {
+        Ok(unix_ms_to_six_byte_hex(self.0))
     }
 }
 
-impl<R: Read> ReadExt for BufReader<R> {}
+impl FieldDeserializer<6> for ExchangeTimestampField {
+    type Err = FieldError;
 
-/// The timestamp of exchange (6 byte).
-pub struct ExchangeTimestampRepr(pub i64);
-
-impl ExchangeTimestampRepr {
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Ok(Self::from_bytes(&reader.read_exact_array()?))
-    }
-
-    pub fn from_bytes(bytes: &[u8; 6]) -> Self {
-        Self(six_byte_hex_to_unix_ms(bytes) as i64)
-    }
-
-    pub fn to_bytes(&self) -> [u8; 6] {
-        unix_ms_to_six_byte_hex(self.0 as u64)
+    fn deserialize(src: &[u8; 6]) -> Result<Self, Self::Err> {
+        Ok(Self(six_byte_hex_to_unix_ms(src)))
     }
 }
 
 /// The timestamp when a message such as order and trade received.
-pub struct ReceivedTimestampRepr(pub u64);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ReceivedTimestampField(pub u64);
 
-impl ReceivedTimestampRepr {
+impl ReceivedTimestampField {
     /// Create a new `ReceivedTimestamp` from the current time.
-    pub fn try_new_from_now() -> StructureResult<Self> {
+    pub fn new_from_now() -> FieldResult<Self> {
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?;
 
         debug_assert!(u64::try_from(now.as_millis()).is_ok());
@@ -63,193 +53,225 @@ impl ReceivedTimestampRepr {
 
         Ok(Self(now_sec))
     }
+}
 
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Ok(Self::from_bytes(&reader.read_exact_array()?))
+impl FieldSerializer<6> for ReceivedTimestampField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 6], Self::Err> {
+        Ok(unix_ms_to_six_byte_hex(self.0))
     }
+}
 
-    pub fn from_bytes(bytes: &[u8; 6]) -> Self {
-        Self(six_byte_hex_to_unix_ms(bytes))
-    }
+impl FieldDeserializer<6> for ReceivedTimestampField {
+    type Err = FieldError;
 
-    pub fn to_bytes(&self) -> [u8; 6] {
-        unix_ms_to_six_byte_hex(self.0)
+    fn deserialize(src: &[u8; 6]) -> Result<Self, Self::Err> {
+        Ok(Self(six_byte_hex_to_unix_ms(src)))
     }
 }
 
 /// The exchange type of a message.
-pub struct ExchangeTypeRepr(pub Exchange);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ExchangeTypeField(pub Exchange);
 
-impl ExchangeTypeRepr {
-    pub fn try_from_str(str: &str) -> StructureResult<Self> {
+impl ExchangeTypeField {
+    pub fn from_str(str: &str) -> FieldResult<Self> {
         let exchange = Exchange::from_str(str)
-            .map_err(|_| StructureError::UnimplementedExchange(Either::Left(str.to_string())))?;
+            .map_err(|_| FieldError::UnimplementedExchange(Either::Left(str.to_string())))?;
         Ok(Self(exchange))
     }
+}
 
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Self::try_from_bytes(&reader.read_exact_array()?)
+impl FieldSerializer<1> for ExchangeTypeField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
+        Ok([self.0 as u8])
     }
+}
 
-    pub fn try_from_bytes(bytes: &[u8; 1]) -> StructureResult<Self> {
-        let bit = bytes[0] as usize;
+impl FieldDeserializer<1> for ExchangeTypeField {
+    type Err = FieldError;
+
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        let bit = src[0] as usize;
         let name = Exchange::from_repr(bit)
-            .ok_or(StructureError::UnimplementedExchange(either::Right(bit)))?;
+            .ok_or(FieldError::UnimplementedExchange(either::Right(bit)))?;
 
         Ok(Self(name))
-    }
-
-    pub fn to_bytes(&self) -> [u8; 1] {
-        [self.0 as u8]
     }
 }
 
 /// The market type of a message.
-pub struct MarketTypeRepr(pub MarketType);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct MarketTypeField(pub MarketType);
 
-impl MarketTypeRepr {
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Ok(Self::from_bytes(&reader.read_exact_array()?))
+impl FieldSerializer<1> for MarketTypeField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
+        let bit = *MARKET_TYPE_BIT.get_by_left(&self.0).unwrap_or(&0);
+
+        Ok([bit])
     }
+}
 
-    pub fn from_bytes(bytes: &[u8; 1]) -> Self {
-        let bit = bytes[0];
+impl FieldDeserializer<1> for MarketTypeField {
+    type Err = FieldError;
+
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        let bit = src[0];
 
         let name = MARKET_TYPE_BIT
             .get_by_right(&bit)
             .unwrap_or(&MarketType::Unknown);
 
-        Self(*name)
-    }
-
-    pub fn to_bytes(&self) -> [u8; 1] {
-        let bit = *MARKET_TYPE_BIT.get_by_left(&self.0).unwrap_or(&0);
-
-        [bit]
+        Ok(Self(*name))
     }
 }
 
 /// The type of a message.
-pub struct MessageTypeRepr(pub MessageType);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct MessageTypeField(pub MessageType);
 
-impl MessageTypeRepr {
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Ok(Self::from_bytes(&reader.read_exact_array()?))
-    }
+impl FieldSerializer<1> for MessageTypeField {
+    type Err = FieldError;
 
-    pub fn from_bytes(bytes: &[u8; 1]) -> Self {
-        Self(bit_deserialize_message_type(bytes[0]))
-    }
-
-    pub fn to_bytes(&self) -> [u8; 1] {
-        [bit_serialize_message_type(self.0)]
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
+        Ok([bit_serialize_message_type(self.0)])
     }
 }
 
-/// The representation of [`TradeSide`].
-pub struct TradeSideRepr(pub TradeSide);
+impl FieldDeserializer<1> for MessageTypeField {
+    type Err = FieldError;
 
-impl TradeSideRepr {
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Self::try_from_bytes(&reader.read_exact_array()?)
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        Ok(Self(bit_deserialize_message_type(src[0])))
     }
+}
 
-    pub fn try_from_bytes(bytes: &[u8; 1]) -> StructureResult<Self> {
-        Ok(Self(bit_deserialize_trade_side(bytes[0])?))
+/// The [`TradeSide`] of a message.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TradeSideField(pub TradeSide);
+
+impl FieldSerializer<1> for TradeSideField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
+        Ok([bit_serialize_trade_side(self.0)])
     }
+}
 
-    pub fn to_bytes(&self) -> [u8; 1] {
-        [bit_serialize_trade_side(self.0)]
+impl FieldDeserializer<1> for TradeSideField {
+    type Err = FieldError;
+
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        Ok(Self(bit_deserialize_trade_side(src[0])?))
     }
 }
 
 /// Exchange-specific trading symbol or id, recognized by RESTful API.
 pub type Symbol = u16;
 /// Unified pair, base/quote, e.g., `BTC/USDT`.
-pub type Pair<'a> = &'a str;
+pub type Pair = String;
 
 /// The symbol of a message.
-pub struct SymbolPairRepr<'a>(pub Symbol, pub Pair<'a>);
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct SymbolPairField(pub Symbol, pub Pair);
 
-impl<'a> SymbolPairRepr<'a> {
-    pub fn from_pair(pair: &'a str) -> Self {
+impl SymbolPairField {
+    pub fn from_pair(pair: &str) -> Self {
         let symbol = *SYMBOL_PAIR.get_by_right(&pair).unwrap_or(&0);
 
-        Self(symbol, pair)
+        Self(symbol, pair.to_string())
     }
+}
 
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Ok(Self::from_bytes(&reader.read_exact_array()?))
+impl FieldSerializer<2> for SymbolPairField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 2], Self::Err> {
+        Ok(self.0.to_be_bytes())
     }
+}
 
-    pub fn from_bytes(bytes: &[u8; 2]) -> Self {
-        let symbol: Symbol = u16::from_be_bytes(*bytes);
-        let pair: Pair = SYMBOL_PAIR.get_by_left(&symbol).unwrap_or(&"UNKNOWN");
+impl FieldDeserializer<2> for SymbolPairField {
+    type Err = FieldError;
 
-        Self(symbol, pair)
-    }
+    fn deserialize(src: &[u8; 2]) -> Result<Self, Self::Err> {
+        let symbol: Symbol = u16::from_be_bytes(*src);
+        let pair: Pair =
+            SYMBOL_PAIR.get_by_left(&symbol).unwrap_or(&"UNKNOWN").to_string();
 
-    pub fn to_bytes(self) -> [u8; 2] {
-        self.0.to_be_bytes()
+        Ok(Self(symbol, pair))
     }
 }
 
 /// The info type (`asks` or `bids`) of a message.
-pub struct InfoTypeRepr(pub InfoType);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct InfoTypeField(pub InfoType);
 
-impl InfoTypeRepr {
-    pub fn try_from_str(str: &str) -> StructureResult<Self> {
+impl InfoTypeField {
+    pub fn from_str(str: &str) -> FieldResult<Self> {
         let exchange = InfoType::from_str(str)
-            .map_err(|_| StructureError::UnimplementedInfoType(Either::Left(str.to_string())))?;
+            .map_err(|_| FieldError::UnimplementedInfoType(Either::Left(str.to_string())))?;
         Ok(Self(exchange))
     }
+}
 
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Self::try_from_bytes(&reader.read_exact_array()?)
+impl FieldSerializer<1> for InfoTypeField {
+    type Err = FieldError;
+
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
+        Ok([self.0 as u8])
     }
+}
 
-    pub fn try_from_bytes(bytes: &[u8; 1]) -> StructureResult<Self> {
-        let bit = bytes[0] as usize;
+impl FieldDeserializer<1> for InfoTypeField {
+    type Err = FieldError;
+
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        let bit = src[0] as usize;
         let name = InfoType::from_repr(bit)
-            .ok_or(StructureError::UnimplementedInfoType(Either::Right(bit)))?;
+            .ok_or(FieldError::UnimplementedInfoType(Either::Right(bit)))?;
 
         Ok(Self(name))
-    }
-
-    pub fn to_bytes(&self) -> [u8; 1] {
-        [self.0 as u8]
     }
 }
 
 /// The period of a message.
-pub struct PeriodRepr<'a>(pub &'a str);
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct PeriodField(pub String);
 
-impl<'a> PeriodRepr<'a> {
-    pub fn try_from_reader(reader: &mut impl ReadExt) -> StructureResult<Self> {
-        Self::try_from_bytes(&reader.read_exact_array()?)
-    }
+impl FieldSerializer<1> for PeriodField {
+    type Err = FieldError;
 
-    pub fn try_from_bytes(bytes: &[u8; 1]) -> StructureResult<Self> {
-        let bit = bytes[0] as u8;
-
-        let name = PERIOD
-            .get_by_right(&bit)
-            .ok_or(StructureError::UnimplementedPeriod(Either::Right(bit)))?;
-
-        Ok(Self(*name))
-    }
-
-    pub fn try_to_bytes(&self) -> StructureResult<[u8; 1]> {
+    fn serialize(&self) -> Result<[u8; 1], Self::Err> {
         let bit = *PERIOD
-            .get_by_left(&self.0)
-            .ok_or_else(|| StructureError::UnimplementedPeriod(Either::Left(self.0.to_string())))?;
+            .get_by_left(self.0.as_str())
+            .ok_or_else(|| FieldError::UnimplementedPeriod(Either::Left(self.0.to_string())))?;
 
         Ok([bit])
     }
 }
 
+impl FieldDeserializer<1> for PeriodField {
+    type Err = FieldError;
+
+    fn deserialize(src: &[u8; 1]) -> Result<Self, Self::Err> {
+        let bit = src[0] as u8;
+
+        let name = PERIOD
+            .get_by_right(&bit)
+            .ok_or(FieldError::UnimplementedPeriod(Either::Right(bit)))?;
+
+        Ok(Self(name.to_string()))
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
-pub enum StructureError {
+pub enum FieldError {
     #[error("data/types error: {0}")]
     DataTypesError(#[from] DataTypesError),
 
@@ -269,41 +291,41 @@ pub enum StructureError {
     UnimplementedPeriod(Either<String, u8>),
 }
 
-pub type StructureResult<T> = Result<T, StructureError>;
+pub type FieldResult<T> = Result<T, FieldError>;
 
 #[cfg(test)]
 mod tests {
     use crate::data::{
-        fields::InfoTypeRepr,
-        types::{Exchange, InfoType},
+        fields::InfoTypeField,
+        types::{Exchange, InfoType}, serializer::FieldDeserializer,
     };
 
-    use super::ExchangeTypeRepr;
+    use super::ExchangeTypeField;
 
     #[test]
     fn test_exchange_expr_from_str() {
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("crypto").unwrap().0,
+            ExchangeTypeField::from_str("crypto").unwrap().0,
             Exchange::Crypto
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("ftx").unwrap().0,
+            ExchangeTypeField::from_str("ftx").unwrap().0,
             Exchange::Ftx
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("binance").unwrap().0,
+            ExchangeTypeField::from_str("binance").unwrap().0,
             Exchange::Binance
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("huobi").unwrap().0,
+            ExchangeTypeField::from_str("huobi").unwrap().0,
             Exchange::Huobi
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("kucoin").unwrap().0,
+            ExchangeTypeField::from_str("kucoin").unwrap().0,
             Exchange::Kucoin
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_str("okx").unwrap().0,
+            ExchangeTypeField::from_str("okx").unwrap().0,
             Exchange::Okx
         );
     }
@@ -311,27 +333,27 @@ mod tests {
     #[test]
     fn test_exchange_expr_from_byte() {
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[1]).unwrap().0,
+            ExchangeTypeField::deserialize(&[1]).unwrap().0,
             Exchange::Crypto
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[2]).unwrap().0,
+            ExchangeTypeField::deserialize(&[2]).unwrap().0,
             Exchange::Ftx
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[3]).unwrap().0,
+            ExchangeTypeField::deserialize(&[3]).unwrap().0,
             Exchange::Binance
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[8]).unwrap().0,
+            ExchangeTypeField::deserialize(&[8]).unwrap().0,
             Exchange::Huobi
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[10]).unwrap().0,
+            ExchangeTypeField::deserialize(&[10]).unwrap().0,
             Exchange::Kucoin
         );
         assert_eq!(
-            ExchangeTypeRepr::try_from_bytes(&[11]).unwrap().0,
+            ExchangeTypeField::deserialize(&[11]).unwrap().0,
             Exchange::Okx
         );
     }
@@ -339,11 +361,11 @@ mod tests {
     #[test]
     fn test_info_type_from_str() {
         assert_eq!(
-            InfoTypeRepr::try_from_str("asks").unwrap().0,
+            InfoTypeField::from_str("asks").unwrap().0,
             InfoType::Asks
         );
         assert_eq!(
-            InfoTypeRepr::try_from_str("bids").unwrap().0,
+            InfoTypeField::from_str("bids").unwrap().0,
             InfoType::Bids
         );
     }
@@ -351,11 +373,11 @@ mod tests {
     #[test]
     fn test_info_expr_from_byte() {
         assert_eq!(
-            InfoTypeRepr::try_from_bytes(&[1]).unwrap().0,
+            InfoTypeField::deserialize(&[1]).unwrap().0,
             InfoType::Asks
         );
         assert_eq!(
-            InfoTypeRepr::try_from_bytes(&[2]).unwrap().0,
+            InfoTypeField::deserialize(&[2]).unwrap().0,
             InfoType::Bids
         );
     }
